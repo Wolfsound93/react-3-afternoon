670fe2e221c993ad69b3ed8f67a795866f7f0c65
ArthrVolfson <arthurvolfson@gmail.com>
2020-02-15 13:04:23 -0600

    done

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-
+import axios from 'axios';
 import './App.css';
+import Post from '../components/Post/Post';
 
 import Header from './Header/Header';
 import Compose from './Compose/Compose';
@@ -13,38 +14,62 @@ class App extends Component {
       posts: []
     };
 
-    this.updatePost = this.updatePost.bind( this );
-    this.deletePost = this.deletePost.bind( this );
-    this.createPost = this.createPost.bind( this );
-  }
-  
-  componentDidMount() {
-
+    this.updatePost = this.updatePost.bind(this);
+    this.deletePost = this.deletePost.bind(this);
+    this.createPost = this.createPost.bind(this);
   }
 
-  updatePost() {
-  
+  componentDidMount = () => {
+    axios.get(`https://practiceapi.devmountain.com/api/posts`).then(results => {
+      this.setState({ posts: results.data });
+    });
+  };
+
+  updatePost = (id, text) => {
+    axios
+      .put(`https://practiceapi.devmountain.com/api/posts?id=${id}`, { text })
+      .then(results => {
+        this.setState({ posts: results.data });
+      });
+  };
+
+  deletePost(id) {
+    axios
+      .delete(`https://practiceapi.devmountain.com/api/posts?id=${id}`)
+      .then(results => {
+        this.setState({ posts: results.data });
+      });
   }
 
-  deletePost() {
-
-  }
-
-  createPost() {
-
+  createPost(text) {
+    axios
+      .post('https://practiceapi.devmountain.com/api/posts', { text })
+      .then(results => {
+        this.setState({ posts: results.data });
+      });
   }
 
   render() {
     const { posts } = this.state;
-
     return (
-      <div className="App__parent">
+      <div className='App__parent'>
         <Header />
 
-        <section className="App__content">
+        <section className='App__content'>
+          <Compose createPostFn={this.createPost} />
 
-          <Compose />
-          
+          {posts.map(post => {
+            return (
+              <Post
+                key={post.id}
+                text={post.text}
+                date={post.date}
+                id={post.id}
+                updatePostFn={this.updatePost}
+                deletePostFn={this.deletePost}
+              />
+            );
+          })}
         </section>
       </div>
     );
--- src/components/Compose/Compose.js
+++ src/components/Compose/Compose.js
@@ -8,20 +8,24 @@ import './Compose.css';
 export default class Compose extends Component {
   constructor() {
     super();
-    
+
     this.state = {
       text: ''
     };
 
-    this.createPost = this.createPost.bind( this );
+    this.createPost = this.createPost.bind(this);
   }
 
-  updateText( text ) {
+  updateText(text) {
     this.setState({ text });
   }
 
   createPost() {
-
+    // console.log('hey');
+    const { text } = this.state;
+    const { createPostFn } = this.props;
+    createPostFn(text);
+    console.log(this.setState({ text: '' }));
   }
 
   render() {
@@ -29,25 +33,25 @@ export default class Compose extends Component {
     const { text } = this.state;
 
     return (
-      <section className="Compose__parent">
-        <div className="Compose__top">
-
-          <div className="Compose__profile-picture">
+      <section className='Compose__parent'>
+        <div className='Compose__top'>
+          <div className='Compose__profile-picture'>
             <ProfileIcon />
           </div>
 
           {/* This is where you type the message for your new post */}
-          <input className="Compose__input"
-                 placeholder="What's on your mind?"
-                 value={ text }
-                 onChange={ ( e ) => this.updateText( e.target.value ) } />
-
+          <input
+            className='Compose__input'
+            placeholder="What's on your mind?"
+            value={text}
+            onChange={e => this.updateText(e.target.value)}
+          />
         </div>
 
-        <div className="Compose__bottom">
-          <button onClick={ this.createPost }>Compose</button>
+        <div className='Compose__bottom'>
+          <button onClick={this.createPost}>Compose</button>
         </div>
       </section>
-    )
+    );
   }
 }
